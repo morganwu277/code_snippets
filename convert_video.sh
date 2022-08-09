c24e0969175a972d695477b332b6d1f41610ee05
Morgan Wu <12993308+morganwu277@users.noreply.github.com>
2022-08-08 23:29:33 -0400

    compress video using webm format

---
+++ convert_video.sh
@@ -0,0 +1,19 @@
+```sh
+# help you to compress a video using webm format
+
+#!/bin/sh
+
+input=${1-""}
+output=${2-"output.webm"}
+
+usage() {
+  printf "Please input correct params: \n\t$0 input.mov [output.webm]\n"
+  exit 1
+}
+
+[[ "$input" != "" ]] || usage
+
+echo "Starting to convert $input to $output"
+ffmpeg -i "$input" -vf scale=1920:-1 -vcodec libvpx-vp9 -an $output
+echo "please checkout $output"
+```
